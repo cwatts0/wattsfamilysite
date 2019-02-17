09a1de3d017a9feeadf08ff24aef69414999b7cc
chris <cwatts0@gmail.com>
2019-02-17 11:49:22 -0500

    First code load

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Container, Row, Col } from 'reactstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+            <div className="App">
+              <div class="column">
+                <div class="menuHeader">Does a menu need a header?</div>
+                <ul class="menuItem">
+                  <li><a>Chris</a></li>
+                  <li>Anica</li>
+                  <li>Will</li>
+                  <li>John</li>
+                  <li>James</li>
+                </ul>
+              </div>
+              <div class="App-header">
+                  <header>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                      Watts Family Site
+                    </p>
+                  
+                  </header>
+              </div> 
+              Powered by React    
+            </div>
+    );
+  }
+}
+
+export default App;
